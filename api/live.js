46b2ce4db158fc52117e02aedc84ec19adff05cf
sushimikado <mallowage@gmail.com>
2026-05-04 04:07:20 +0900

    Update live.js

--- api/live.js
+++ api/live.js
@@ -1,8 +1,9 @@
 export default async function handler(req, res) {
   try {
     const NOTION_TOKEN = process.env.NOTION_TOKEN;
+    const DATABASE_ID = process.env.NOTION_DATABASE_ID;
 
-    const r = await fetch("https://api.notion.com/v1/users/me", {
+    const r = await fetch(`https://api.notion.com/v1/databases/${DATABASE_ID}`, {
       headers: {
         "Authorization": `Bearer ${NOTION_TOKEN}`,
         "Notion-Version": "2022-06-28"
